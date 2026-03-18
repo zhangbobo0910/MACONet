bebb6e1773021e15f06bcae02b653389cc8b1e59
songbo zhang <1516211443@qq.com>
2026-03-18 15:22:42 +0800

    Add files via upload

---
+++ train_arg_04.sh
@@ -0,0 +1,1 @@
+python train.py  -n 100 -d ace2004 --lr 2e-5 --cnn_dim 120 -b 16 --cnn_depth 1 --n_head 4--theta 0.35 --logit_drop 0.25  --seed 20010508
---
+++ train_arg_05.sh
@@ -0,0 +1,1 @@
+python train.py  -n 80 -d ace2005 --lr 2e-5 --cnn_dim 120 -b 16 --cnn_depth 1 --n_head 4 --logit_drop 0.1  --seed 20010508 --n_layer 2
